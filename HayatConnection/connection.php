9fffa24e3ffa82dd148164244d2b0ce017cb06b4
SBAI SALAH <88663177+Sbai-Salah@users.noreply.github.com>
2022-05-29 15:47:23 +0100

    DB pushed

---
+++ HayatConnection/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "hayatdb";
+
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
+
+    die("failed to connect!");
+}
+
+?>
